c44a08f06b1caf9b478c564ffe150cfe60707b86
Tripti <taheem700@gmail.com>
2025-09-21 01:11:20 +0600

    Trishna

---
+++ Grace_cil/Controllers/Production/Transaction/T16020Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grace_cil.Controllers.Production.Transaction
+{
+    public class T16020Controller : Controller
+    {
+        // GET: T16020
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
