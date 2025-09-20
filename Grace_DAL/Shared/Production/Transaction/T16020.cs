9ba7bf78bd201770634431dbe5bec5df62036ec8
Tripti <taheem700@gmail.com>
2025-09-21 01:24:37 +0600

    T

---
+++ Grace_DAL/Shared/Production/Transaction/T16020.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace_DAL.Shared.Production.Transaction
+{
+    class T16020
+    {
+    }
+}
